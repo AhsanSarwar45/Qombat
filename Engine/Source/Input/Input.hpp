638ed4568f542332dcfcd6f00edca3cc5334acf9
AhsanSarwar45 <ahsanrockzs@gmail.com>
2021-05-20 23:02:13 +0500

    Fixed missing includes in Input.hpp

--- Engine/Source/Input/Input.hpp
+++ Engine/Source/Input/Input.hpp
@@ -11,8 +11,8 @@ namespace QMBT
 		static bool IsKeyPressed(KeyCode key);
 		static bool IsMouseButtonClicked(MouseCode button);
 		static bool IsMouseButtonDoubleClicked(MouseCode button);
-		static glm::vec2 GetMousePosition();
-		static float GetMouseX();
-		static float GetMouseY();
+		// static glm::vec2 GetMousePosition();
+		// static float GetMouseX();
+		// static float GetMouseY();
 	};
 } // namespace QMBT
